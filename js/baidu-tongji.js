b1d90a53506cbf2c4e7e20231fcf53411ffaf037
Way Lau <waylau521@gmail.com>
2015-02-24 16:29:40 +0800

    add  	 baidu-tongji.js

---
+++ js/baidu-tongji.js
@@ -0,0 +1,8 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?5f2f156d249ea2f65f8f1a2e5004deca";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
